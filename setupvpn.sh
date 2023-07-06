e7fdea41a92316865c82d16b75e2b4fdc5f12060
Denis Uliankin <denis.uliankin@gmail.com>
2023-07-06 09:50:49 +0300

    dz 2

---
+++ setupvpn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "deb http://repo.pritunl.com/stable/apt focal main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+curl https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo apt-key add -
+echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+wget http://es.archive.ubuntu.com/ubuntu/pool/main/libf/libffi/libffi7_3.3-4_amd64.deb
+sudo dpkg -i libffi7_3.3-4_amd64.deb
+apt update
+apt -y install wireguard wireguard-tools wget vim curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release libssl1.1
+ufw disable
+apt -y install pritunl mongodb-org
+systemctl enable mongod pritunl
+systemctl start mongod pritunl
